3b94c2213f54fbef30707ddb4a6d15193bd26a3e
Glushkov Alex <avgl@mail.ru>
2024-08-13 10:28:55 +0300

    Коммит домашнего задания 24.1 DRF

---
+++ lms/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.forms import BooleanField
+
+# from main.models import Client, Mailing, Notification, Attempt
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+
+
+#
+# class MailingForm(StyleFormMixin, forms.ModelForm):
+#     class Meta:
+#         model = Mailing
+#         exclude = ('owner', 'is_active',)
+#
+# class ClientForm(StyleFormMixin, forms.ModelForm):
+#     class Meta:
+#         model = Client
+#         exclude = ('owner',)
+#
+# class NotificationForm(StyleFormMixin, forms.ModelForm):
+#     class Meta:
+#         model = Notification
+#         exclude = ('owner',)
+#
+# class MailingActivationForm(StyleFormMixin,forms.ModelForm):
+#     class Meta:
+#         model = Mailing
+#         fields = ("is_active",)
