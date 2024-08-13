039efbc1e0398b6fdc1eed05d2de30c12052f056
Glushkov Alex <avgl@mail.ru>
2024-08-13 10:28:55 +0300

    Коммит домашнего задания 24.2

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from users.models import User
+from users.models import User, Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, source='payment_set')
+
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username','email','phone','town','payments')
--- users/views.py
+++ users/views.py
@@ -15,12 +15,14 @@ from django.urls import reverse_lazy, reverse
 from django.core.mail import send_mail
 
 from rest_framework import viewsets, generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 from config.settings import EMAIL_HOST_USER
 
 from users.forms import UserRegisterForm, UserProfileForm, UserPasswordRecoveryForm, UserUpdateForm
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
 
 
 class RegisterView(CreateView):
@@ -133,3 +135,33 @@ class UserUpdateView(LoginRequiredMixin,PermissionRequiredMixin, UpdateView):
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+# class PaymentViewSet(viewsets.ModelViewSet):
+#     serializer_class = PaymentSerializer
+#     queryset = Payment.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['course', 'lesson', 'payment_type']
+    ordering_fields = ['payment_date']
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
+
+
+class PaymentUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+
+class PaymentRetriveAPIView(generics.RetrieveAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+
+
+class PaymentDestroyAPIView(generics.DestroyAPIView):
+    queryset = Payment.objects.all()
